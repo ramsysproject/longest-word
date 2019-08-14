9f2d6ebb91c6e138228113f6464b803db3bbe3b4
ramsysproject <m.ramirez05@gmail.com>
2019-08-14 15:59:32 -0300

    Add punctuation removal logic

--- src/main/java/com/emramirez/longestword/logic/StringAnalyzer.java
+++ src/main/java/com/emramirez/longestword/logic/StringAnalyzer.java
@@ -3,6 +3,19 @@ package com.emramirez.longestword.logic;
 import java.util.Arrays;
 import java.util.Comparator;
 
+/**
+ * Challenge
+ * Have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty.
+ * Sample Test Cases
+ * Input:"fun&!! time"
+ *
+ * Output:time
+ *
+ *
+ * Input:"I love dogs"
+ *
+ * Output:love
+ */
 public class StringAnalyzer {
 
     private static final String EMPTY_STRING = "";
@@ -12,6 +25,7 @@ public class StringAnalyzer {
             return EMPTY_STRING;
         }
 
+        input = input.replaceAll("\\p{Punct}", "");
         return Arrays.stream(input.split("\\s+"))
                 .sorted(Comparator.comparingInt(String::length).reversed())
                 .findFirst().orElse(EMPTY_STRING);
